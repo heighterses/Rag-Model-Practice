b7d139331244e89b76cdb75ddbcd7e27e633e041
M. Abdullah Bin Aftab <heighter79@gmail.com>
2024-02-17 11:28:00 +0500

    adding splitting methods in code

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from langchain_community.document_loaders import PyPDFLoader
 import os
 import openai
 import sys
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
 
 sys.path.append('../..')
 from dotenv import load_dotenv, find_dotenv
 
-# Correcting the load_dotenv function call
 load_dotenv(find_dotenv())
 
 openai.api_key = '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'
@@ -14,3 +14,7 @@ openai.api_key = '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'
 loader = PyPDFLoader("pdf_files/MachineLearning-Lecture01.pdf")
 pages = loader.load()
 print(len(pages))
+
+page = pages[3]
+content = page.page_content[:500]
+print(content)
